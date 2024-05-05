94e1bd0033032c04c8ceac3042a91050b3ee4e9b
John <codecreator127@gmail.com>
2024-05-05 20:42:09 +1200

    ran prettier

--- .github/ISSUE_TEMPLATE/task-template.md
+++ .github/ISSUE_TEMPLATE/task-template.md
@@ -2,14 +2,14 @@
 name: Task Template
 about: This is to be used as the template for the SESA project
 title: ""
-labels: ''
-assignees: ''
-
+labels: ""
+assignees: ""
 ---
 
 **Description:**
 
 **Criteria:**
+
 - [ ] Criteria 1
 - [ ] Criteria 2
 - [ ] Criteria 3
--- src/components/SesaHeader.tsx
+++ src/components/SesaHeader.tsx
@@ -1,19 +1,35 @@
 import SesaLogo from "../../public/sesaLogo.svg";
 
 export default function SesaHeader() {
-    return (
-        <ul className="bg-primary flex justify-center items-center p-5">
-            <div className="min-w-40 border-solid text-center inline pr-3">
-                <img src={SesaLogo.src} alt="SESA Logo" />
-            </div>
-            <li className="font-sans text-lg text-primary-content inline px-6"><a href="#">Home</a></li>
-            <li className="font-sans text-lg text-primary-content inline px-6"><a href="#">News</a></li>
-            <li className="font-sans text-lg text-primary-content inline px-6"><a href="#">Our Team</a></li>
-            <li className="font-sans text-lg text-primary-content inline px-6"><a href="#">Sponsors</a></li>
-            <li className="font-sans text-lg text-primary-content inline px-6"><a href="#">Careers</a></li>
-            <li className="font-sans text-lg text-primary-content inline px-6"><a href="#">Event Calendar</a></li>
-            <li className="font-sans text-lg text-primary-content inline pl-6 pr-9"><a href="#">Contact</a></li>
-            <button className="bg-accent rounded-full font-sans text-lg text-primary-content inline px-6 py-2" >Join</button>
-        </ul>
-    )
+  return (
+    <ul className="bg-primary flex justify-center items-center p-5">
+      <div className="min-w-40 border-solid text-center inline pr-3">
+        <img src={SesaLogo.src} alt="SESA Logo" />
+      </div>
+      <li className="font-sans text-lg text-primary-content inline px-6">
+        <a href="#">Home</a>
+      </li>
+      <li className="font-sans text-lg text-primary-content inline px-6">
+        <a href="#">News</a>
+      </li>
+      <li className="font-sans text-lg text-primary-content inline px-6">
+        <a href="#">Our Team</a>
+      </li>
+      <li className="font-sans text-lg text-primary-content inline px-6">
+        <a href="#">Sponsors</a>
+      </li>
+      <li className="font-sans text-lg text-primary-content inline px-6">
+        <a href="#">Careers</a>
+      </li>
+      <li className="font-sans text-lg text-primary-content inline px-6">
+        <a href="#">Event Calendar</a>
+      </li>
+      <li className="font-sans text-lg text-primary-content inline pl-6 pr-9">
+        <a href="#">Contact</a>
+      </li>
+      <button className="bg-accent rounded-full font-sans text-lg text-primary-content inline px-6 py-2">
+        Join
+      </button>
+    </ul>
+  );
 }
