8dab694f86b04b795ea6e4b7668ccc82753081f1
tonylxm <126369686+tonylxm@users.noreply.github.com>
2024-05-08 15:11:10 +1200

    Footer completed
    * Full component - logo, social media links (added tiktok) and copyright statement
    * Might need to add other things in the footer later, e.g. links (consitution), back to top button
    
    Take a look at footer.tsx file
    
    Visual testing through live server
    
    @codecreator127 please take a look

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,24 @@
 import SocialMediaButtons from "./social_media_buttons";
+import SesaLogo from "../../public/sesaLogo.svg";
 
 const currentYear = new Date().getFullYear();
 
 export default function Footer() {
   return (
     <footer>
-      <div className="h-32 bg-black text-white opacity-60 shadow-md p-5 justify-center items-center">
-        <div className="h-full flex flex-col-reverse justify-center items-center md:flex-row md:justify-between md:items-center md:flex-wrap md:gap-6">
-          <a href="#" className="px-20 hover:underline justify-center my-2">
-            &copy; {currentYear} localhost
-          </a>
-          <div className="px-20 justify-center my-2">
+      <div className="bg-black text-white opacity-60 shadow-md p-5 justify-center items-center">
+        <div className="h-full flex flex-col justify-center items-center md:flex-row md:justify-between md:items-center md:flex-wrap md:gap-6">
+          <div className="min-w-28 border-solid inline mx-16">
+            <a href="#">
+              <img src={SesaLogo.src} alt="SESA Logo" />
+            </a>
+          </div>
+          <div className="py-3 md:px-20">
             <SocialMediaButtons />
           </div>
+        </div>
+        <div className="py-3 text-center md:flex md:items-center md:justify-center">
+          &copy; {currentYear} - Software Engineering Students Association
         </div>
       </div>
     </footer>
