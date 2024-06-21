550d0bb573e671c6130ab6494499985e126c3ff5
tonylxm <126369686+tonylxm@users.noreply.github.com>
2024-06-21 22:11:04 +1200

    refactor: Navigation & Social media buttons

--- src/components/SesaHeader.tsx
+++ src/components/SesaHeader.tsx
@@ -1,36 +1,45 @@
 import Link from "next/link";
 import SesaLogo from "../../public/sesaLogo.svg";
+import { routes } from "../constants/routes";
 
 export default function Header() {
   return (
     <div className="bg-primary flex justify-between items-center p-[20px] px-[150px] mx-auto">
-      <a href="#" className="min-w-40 border-solid text-center inline px-3">
+      <a href="/" className="min-w-40 border-solid text-center inline px-3">
         <img src={SesaLogo.src} alt="SESA Logo" />
       </a>
 
       <ul className="flex justify-center items-center p-5">
-        <li className="font-sans text-lg text-primary-content inline px-6">
-          <Link href="/">Home</Link>
-        </li>
-        <li className="font-sans text-lg text-primary-content inline px-6">
-          <Link href="/our-team">Our Team</Link>
-        </li>
-        <li className="font-sans text-lg text-primary-content inline px-6">
-          <Link href="/sponsors">Sponsors</Link>
-        </li>
-        <li className="font-sans text-lg text-primary-content inline px-6">
-          <Link href="https://linktr.ee/sesa.uoa">Linktree</Link>
-        </li>
-        <li className="font-sans text-lg text-primary-content inline px-6">
-          <Link href="/event-calendar">Event Calendar</Link>
-        </li>
-        <li className="font-sans text-lg text-primary-content inline pl-6 pr-9">
-          <Link href="/contact">Contact</Link>
-        </li>
-        <button className="bg-accent rounded-full font-sans text-lg text-primary-content inline px-6 py-[6px]">
+        {routes.map((route) => {
+          const { id, name, href } = route;
+          return (
+            <li key={id} className="font-sans text-lg text-primary-content whitespace-nowrap inline px-6">
+              <Link href={href} className="flex items-center gap-1 transition-all">
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+        <button key="join-button" className="bg-accent rounded-full font-sans text-lg text-primary-content inline transition-transform duration-200 transform hover:scale-110 px-6 py-[6px] ml-6">
           Join
         </button>
       </ul>
+
+      {/* <button className="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="inline-block w-8 h-8 stroke-white"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          ></path>
+        </svg>
+      </button> */}
     </div>
   );
 }
--- src/components/SocialMediaButtons.tsx
+++ src/components/SocialMediaButtons.tsx
@@ -1,81 +1,25 @@
 import Image from "next/image";
-
-import facebook from "../../public/social-media-icons/facebook.svg";
-import instagram from "../../public/social-media-icons/instagram.svg";
-import linkedin from "../../public/social-media-icons/linkedin.svg";
-import discord from "../../public/social-media-icons/discord.svg";
-import tiktok from "../../public/social-media-icons/tiktok.svg";
+import { socialMedia } from "../constants/socialMedia";
 
 export default function SocialMediaButtons() {
   const iconSize = 40;
 
   return (
     <div className="flex items-center justify-center">
-      <a
-        href="https://www.facebook.com/uoasesa"
-        className="mr-4 transition-transform duration-200 transform hover:scale-105"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src={facebook}
-          width={iconSize}
-          height={iconSize}
-          alt="facebook-icon"
-        />
-      </a>
-      <a
-        href="https://www.instagram.com/sesa.uoa"
-        className="mr-4 transition-transform duration-200 transform hover:scale-105"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src={instagram}
-          width={iconSize}
-          height={iconSize}
-          alt="instagram-icon"
-        />
-      </a>
-      <a
-        href="https://www.linkedin.com/company/ausesa"
-        className="mr-4 transition-transform duration-200 transform hover:scale-105"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src={linkedin}
-          width={iconSize}
-          height={iconSize}
-          alt="linkedin-icon"
-        />
-      </a>
-      <a
-        href="https://discord.gg/93AW5tMSBc"
-        className=" mr-4 transition-transform duration-200 transform hover:scale-105"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src={discord}
-          width={iconSize}
-          height={iconSize}
-          alt="discord-icon"
-        />
-      </a>
-      <a
-        href="https://www.tiktok.com/@sesauoa"
-        className="transition-transform duration-200 transform hover:scale-105"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Image
-          src={tiktok}
-          width={iconSize}
-          height={iconSize}
-          alt="tiktok-icon"
-        />
-      </a>
+      {socialMedia.map((media) => {
+        const { id, href, src, alt } = media;
+        return (
+          <a
+            key={id}
+            href={href}
+            className="mr-4 transition-transform duration-200 transform hover:scale-110"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={src} width={iconSize} height={iconSize} alt={alt} />
+          </a>
+        );
+      })}
     </div>
   );
 }
