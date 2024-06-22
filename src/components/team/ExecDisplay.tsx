3f09803f02ea54d8736e1fbc095b8aec8fa96180
alux444 <aluux999@gmail.com>
2024-06-22 15:48:26 +1200

    Removed test_image.jpeg

--- src/components/team/ExecDisplay.tsx
+++ src/components/team/ExecDisplay.tsx
@@ -1,6 +1,7 @@
 import { TeamInfo } from "@/constants/TeamInfo";
 
 const ExecDisplay = () => {
+  const YEAR = "2024";
   return (
     <div className="parent-container">
       <div className="executives-container flex flex-row gap-2 flex-wrap items-center justify-center">
@@ -8,7 +9,7 @@ const ExecDisplay = () => {
           <div className="executive relative w-[200px] h-[200px]" key={index}>
             <img
               className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
-              src={`/team-images/2024/${executive.imageName}.jpg`}
+              src={`/team-images/${YEAR}/${executive.imageName}.jpg`}
               alt={executive.name}
             />
             <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 hover:opacity-100 transition-opacity duration-500 flex items-center justify-center">
