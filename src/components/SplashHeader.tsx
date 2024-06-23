23fccb985ba5bca628617b78aa74b1d1ac84bc3b
tonylxm <126369686+tonylxm@users.noreply.github.com>
2024-06-23 16:55:08 +1200

    feat: Navigation
    * Nav hover animations
    * Nav responsiveness with hamburger menu
    * SideNav mobile animations
    * README update

--- src/components/SplashHeader.tsx
+++ src/components/SplashHeader.tsx
@@ -12,19 +12,21 @@ const SplashHeader: React.FC<SplashHeaderProps> = ({
   bodyText,
 }) => {
   return (
-    <div className="relative mx-auto py-32">
+    <div className=" mx-auto py-32">
       <div className="flex items-center px-5">
-        <div className="w-2/3">
+        <div className="w-4/5 lg:w-2/3">
           <h1 className="text-accent text-5xl md:text-6xl xl:text-7xl text-shadow font-bold">
             {headerText}
           </h1>
-          <p className="text-white md:text-xl xl:text-2xl text-shadow pt-2">{bodyText}</p>
+          <p className="text-white md:text-xl xl:text-2xl text-shadow pt-2">
+            {bodyText}
+          </p>
           <div className="float-left pt-6">
             <SocialMediaButtons />
           </div>
         </div>
       </div>
-      <div className="hidden lg:flex absolute top-0 right-0 h-full w-1/2 p-0 items-center overflow-hidden">
+      <div className="hidden lg:flex absolute top-0 right-0 h-2/5 w-1/2 p-0 my-40 items-center overflow-hidden">
         <ThreeScene />
       </div>
     </div>
