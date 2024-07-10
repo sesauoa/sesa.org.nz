a5de6b4eaf118912347131a3dcc28e487d5ced67
oorjagandhi <oorjagandhi@gmail.com>
2024-07-10 22:57:03 +1200

    Added our 2024 sponsors section

--- src/app/(pages)/sponsors/page.tsx
+++ src/app/(pages)/sponsors/page.tsx
@@ -1,7 +1,55 @@
-export const metadata = {
-  title: "SESA - Sponsors",
+import React from 'react';
+import SilverSponsorCard from "@/components/SilverSponsorCard";
+import GoldSponsorCard from "@/components/GoldSponsorCard";
+
+
+interface Sponsor {
+  name: string;
+  logo: string;
+  link: string;
+}
+
+const goldSponsors: Sponsor[] = [
+  { name: "Jane Street", logo: "sponsors-images/jane-st.png", link: "https://www.janestreet.com/join-jane-street/overview/"}
+];
+
+const silverSponsors: Sponsor[] = [
+  { name: "IMC Trading", logo: "sponsors-images/IMC.png", link: "https://careers.imc.com/us/en" },
+  { name: "Atlassian", logo: "sponsors-images/atlassian.png", link: "https://www.atlassian.com/company/careers" },
+  { name: "Canva", logo: "sponsors-images/Canva.svg", link: "https://www.lifeatcanva.com/en/jobs/" },
+  { name: "Serato", logo: "sponsors-images/serato.png", link: "https://serato.com/careers" },
+  { name: "Vista", logo: "sponsors-images/vista.png", link: "https://www.vista.co/careers/" },
+  { name: "Sandfield", logo: "sponsors-images/sandfield.png", link: "https://www.sandfield.co.nz/careers/interns-graduates" },
+  { name: "Emergency Q", logo: "sponsors-images/EmergencyQ.svg", link: "https://www.emergencyq.com/" }
+];
+
+const SponsorsPage: React.FC = () => {
+  return (
+    <div className="page-padding bg-gray-900">
+      <h1 className="page-title text-center">Our 2024 Sponsors</h1>
+      <div className="mt-8">
+        <h2 className="text-2xl font-bold text-center mb-4">Gold Sponsors</h2>
+        <div className="mx-auto w-20 my-2 bg-accent h-[2px]"></div>
+        <div className="flex flex-wrap justify-center gap-4 max-w-6xl mx-auto">
+          {goldSponsors.map((sponsor, index) => (
+            <div key={index} className="px-12 py-2">
+            <GoldSponsorCard key={index} name={sponsor.name} logo={sponsor.logo} link={sponsor.link}/>
+            </div>
+          ))}
+        </div>
+        <h2 className="text-2xl font-bold text-center mt-16 mb-4">Silver Sponsors</h2>
+        <div className="mx-auto w-20 my-2 bg-accent h-[2px]"></div>
+        <div className="flex flex-wrap justify-center gap-4 max-w-6xl mx-auto">
+          {silverSponsors.map((sponsor, index) => (
+            <div key={index} className="px-12 py-2">
+              <SilverSponsorCard key={index} name={sponsor.name} logo={sponsor.logo} link={sponsor.link}/>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default function SponsorsPage() {
-  return <div></div>;
-}
+export default SponsorsPage;
+
---
+++ src/components/GoldSponsorCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../app/globals.css';
+
+interface SponsorCardProps {
+  name: string;
+  logo: string;
+  link: string;
+}
+
+const SponsorCard: React.FC<SponsorCardProps> = ({ name, logo, link }) => {
+  return (
+    <a href={link} target="_blank" rel="noopener noreferrer" className="w-32 h-32">
+      <div className="bg-gray-900 w-64 h-32 flex items-center justify-center
+                      transform hover:scale-105 hover:shadow-2xl transition-transform transition-shadow duration-300">
+        <img src={logo} alt={name} className="h-full w-full object-contain" />
+      </div>
+    </a>
+  );
+};
+
+export default SponsorCard;
