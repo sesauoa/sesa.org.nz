541c564ae3942b2225bfe5a9e37f23511506054e
oorjagandhi <oorjagandhi@gmail.com>
2024-08-15 23:42:48 +1200

    moved sponsors into seperate component

---
+++ src/constants/sponsors.tsx
@@ -0,0 +1,45 @@
+export const goldSponsors = [
+    {
+      name: "Jane Street",
+      logo: "sponsors-images/jane-st.png",
+      link: "https://www.janestreet.com/join-jane-street/overview/",
+    },
+  ];
+  
+  export const silverSponsors = [
+    {
+      name: "IMC Trading",
+      logo: "sponsors-images/IMC.png",
+      link: "https://careers.imc.com/us/en",
+    },
+    {
+      name: "Atlassian",
+      logo: "sponsors-images/atlassian.png",
+      link: "https://www.atlassian.com/company/careers",
+    },
+    {
+      name: "Canva",
+      logo: "sponsors-images/Canva.svg",
+      link: "https://www.lifeatcanva.com/en/jobs/",
+    },
+    {
+      name: "Serato",
+      logo: "sponsors-images/serato.png",
+      link: "https://serato.com/careers",
+    },
+    {
+      name: "Vista",
+      logo: "sponsors-images/vista.png",
+      link: "https://www.vista.co/careers/",
+    },
+    {
+      name: "Sandfield",
+      logo: "sponsors-images/sandfield.png",
+      link: "https://www.sandfield.co.nz/careers/interns-graduates",
+    },
+    {
+      name: "Emergency Q",
+      logo: "sponsors-images/EmergencyQ.svg",
+      link: "https://www.emergencyq.com/",
+    },
+  ];
