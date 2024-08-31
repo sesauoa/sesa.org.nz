22a781c661ccdfed87eabe20ad165e01fc943355
oorjagandhi <oorjagandhi@gmail.com>
2024-09-01 10:28:14 +1200

    updated sesa member count

--- src/components/SponsorStats.tsx
+++ src/components/SponsorStats.tsx
@@ -6,7 +6,7 @@ const StatisticsSection: React.FC = () => {
       <div className="text-center my-8">
         <div className="flex justify-center space-x-8">
           <div className="w-[130px]">
-            <h2 className="text-2xl">450+</h2>
+            <h2 className="text-2xl">500+</h2>
             <p className="">Registered members</p>
           </div>
           <div className="w-[130px]">
