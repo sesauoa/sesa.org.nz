bc7675033f8d2f2b81417de869623269fc756d7b
oorjagandhi <oorjagandhi@gmail.com>
2024-09-01 11:11:43 +1200

    fixed spacing issues

--- src/components/SponsorUsCards.tsx
+++ src/components/SponsorUsCards.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SponsorUsCards: React.FC = () => {
   return (
-    <div className="text-center my-8 flex flex-wrap justify-center space-x-4 space-y-4 md:space-x-8 md:space-y-0">
+    <div className="text-center my-8 flex flex-wrap justify-center gap-4 md:space-x-8 w-full">
       {[
         {
           imgSrc: "sponsors-images/showcase-opportunities.jpg",
@@ -28,14 +28,14 @@ const SponsorUsCards: React.FC = () => {
       ].map((card, index) => (
         <div
           key={index}
-          className="max-w-xs bg-white rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden"
+          className="max-w-xs w-full bg-white rounded-2xl shadow-md hover:shadow-lg transition-shadow duration-300 overflow-hidden"
         >
           <img
             className="w-full h-48 object-cover"
             src={card.imgSrc}
             alt={card.alt}
           />
-          <div className="px-4 py-8">
+          <div className="px-4 py-8 mt-4 gap-8">
             <h2 className="text-xl font-bold mb-2">{card.title}</h2>
             <p className="text-gray-600">{card.description}</p>
           </div>
