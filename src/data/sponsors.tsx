4e38be21a1d8ddf98b6575b7d1c323f71134aa4b
tonylxm <126369686+tonylxm@users.noreply.github.com>
2025-03-10 18:59:19 +1300

    refactor: Update 2025 sponsors

--- src/data/sponsors.tsx
+++ src/data/sponsors.tsx
@@ -1,45 +1,60 @@
 export const goldSponsors = [
   {
     name: "Jane Street",
-    logo: "sponsors-images/2024/jane-st.png",
+    logo: "sponsors-images/logos/jane_street.png",
     link: "https://www.janestreet.com/join-jane-street/overview/",
   },
+  {
+    name: "IMC Trading",
+    logo: "sponsors-images/logos/imc.png",
+    link: "https://www.imc.com/ap/careers/students-graduates",
+  },
+  {
+    name: "Tidy International",
+    logo: "sponsors-images/logos/tidy.svg",
+    link: "https://www.tidyinternational.com/tidy-careers/opportunities",
+  }
 ];
 
 export const silverSponsors = [
   {
-    name: "IMC Trading",
-    logo: "sponsors-images/2024/IMC.png",
-    link: "https://careers.imc.com/us/en",
+    name: "Optiver",
+    logo: "sponsors-images/logos/optiver.png",
+    link: "https://optiver.com/working-at-optiver/early-careers/",
   },
   {
-    name: "Atlassian",
-    logo: "sponsors-images/2024/atlassian.png",
-    link: "https://www.atlassian.com/company/careers",
-  },
-  {
-    name: "Canva",
-    logo: "sponsors-images/2024/Canva.svg",
-    link: "https://www.lifeatcanva.com/en/jobs/",
-  },
-  {
-    name: "Serato",
-    logo: "sponsors-images/2024/serato.png",
-    link: "https://serato.com/careers",
-  },
-  {
-    name: "Vista",
-    logo: "sponsors-images/2024/vista.png",
-    link: "https://www.vista.co/careers/",
+    name: "Citadel Securities",
+    logo: "sponsors-images/logos/citadel_securities.png",
+    link: "https://www.citadelsecurities.com/careers/students/",
   },
   {
     name: "Sandfield",
-    logo: "sponsors-images/2024/sandfield.png",
+    logo: "sponsors-images/logos/sandfield.png",
     link: "https://www.sandfield.co.nz/careers/interns-graduates",
   },
   {
-    name: "Emergency Q",
-    logo: "sponsors-images/2024/EmergencyQ.png",
-    link: "https://www.emergencyq.com/",
+    name: "Atlassian",
+    logo: "sponsors-images/logos/atlassian.png",
+    link: "https://www.atlassian.com/company/careers/earlycareers",
+  },
+  {
+    name: "Susquehanna",
+    logo: "sponsors-images/logos/susquehanna.png",
+    link: "https://careers.sig.com/",
+  },
+  {
+    name: "Serato",
+    logo: "sponsors-images/logos/serato.png",
+    link: "https://serato.com/careers",
+  },
+  {
+    name: "2degrees",
+    logo: "sponsors-images/logos/2degrees.png",
+    link: "https://careers.2degrees.nz/graduates",
+  },
+  {
+    name: "Visa",
+    logo: "sponsors-images/logos/visa.png",
+    link: "https://www.visa.co.nz/careers/university-recruiting.html",
   },
 ];
